cf8e232f288531d4acdcd82fd8680b6c374b1ebd
mickmed <mick2090@gmail.com>
2020-07-03 12:17:35 -0400

    prod url

--- src/js/Services/ApiAuth.js
+++ src/js/Services/ApiAuth.js
@@ -1,5 +1,5 @@
 
-const BASE_URL = process.env.NODE_ENV === 'production' ? 'http://' : 'http://localhost:3000'
+const BASE_URL = process.env.NODE_ENV === 'production' ? 'https://portfolio-mick-server.herokuapp.com' : 'http://localhost:3000'
 
 
 
